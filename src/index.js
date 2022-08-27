7042a03e4acdad36488bd8fcea0fab7355f16153
root <root@RTX3060Ti.localdomain>
2022-08-27 16:14:19 -0400

    winston logger added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 const UserRoute = require('./api/routes/user.route');
+const logger = require('./api/middlewares/Logger');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,4 +13,7 @@ app.use(bodyParser.json());
 app.use('/user', UserRoute);
 app.all('*', (req, res) => res.status(404).json({ "code": 404, "description": "bad request", "message": "endpoint not found" }));
 
-app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.clear();
+  logger.info(`App is running on port ${PORT}`);
+});
