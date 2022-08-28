17932a34a3c0f32681a4ee016fbee4cc208c14ae
root <root@RTX3060Ti.localdomain>
2022-08-28 19:13:27 -0400

    db create table if not exists method on repo level

--- src/api/models/Repository.js
+++ src/api/models/Repository.js
@@ -1,16 +1,52 @@
+const logger = require("../middlewares/Logger");
+
 class Repository {
-  client;
+  dbPool;
   tableName;
-  constructor(client) {
-    this.client = client;
+  entity;
+  constructor(dbPool) {
+    this.dbPool = dbPool;
     this.tableName = `${process.env.TABLE_SCHEMA}.${(this.constructor.name).toLowerCase()}`;
   }
 
-  async query(client, queryString, values) {
-    await client.connect();
-    const res = await client.query(queryString, values);
-    await client.end();
-    return res;
+  async execute(queryString, values) {
+    try {
+      await this.dbPool.connect();
+      const res = await this.dbPool.query(queryString, values);
+      return res;
+    } catch (err) {
+      logger.error(err);
+    }
+  }
+
+  async createTableIfNotExists(dbClient, entity) {
+    const columns = this.entityToSqlColumnList(entity);
+    const queryString = `CREATE TABLE IF NOT EXISTS ${this.tableName} (${columns});`;
+
+    try {
+      await dbClient.connect();
+      await dbClient.query(queryString);
+      return true;
+    } catch (error) {
+      console.error(error.stack);
+      return false;
+    } finally {
+      await dbClient.end();
+    }
+  }
+
+  entityToSqlColumnList(entity) {
+    const columns = [];
+    const columnNames = Object.keys(entity);
+    columnNames.forEach(columnName => {
+      const columnInfo = [];
+      const column = entity[columnName];
+      columnInfo.push(columnName)
+      columnInfo.push(column.dataType);
+      columnInfo.push(column.constraints.join(' '));
+      columns.push(columnInfo.join(' '));
+    });
+    return columns.join(', ');
   }
 
   async save(dto) {
@@ -19,8 +55,12 @@ class Repository {
     keys.forEach(key => { values.push(dto[key]) });
     const valuesCount = keys.map((key, index) => `$${index + 1}`);
     const queryString = `INSERT INTO ${this.tableName} (${keys.join()}) VALUES (${valuesCount}) RETURNING *`;
-    const res = await this.query(this.client, queryString, values);
-    return res.rows[0];
+    try {
+      const res = await this.execute(queryString, values);
+      return res.rows[0];
+    } catch (err) {
+      logger.error(err);
+    }
   }
 }
 
